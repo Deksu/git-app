9cfe7a1f065946f4651e14943ce78cbc2da3b116
Deksu <minxzu@gmail.com>
2018-02-23 10:51:24 +0200

    adding vue-practise

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import tasks from './components/tasks.vue'
+import about from './components/about.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'tasks',
+      component: tasks
+    },
+    {
+      path: '/about/:name',
+      name: 'about',
+      component: about
+    }
+  ]
+})
